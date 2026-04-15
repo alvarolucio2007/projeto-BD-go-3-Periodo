51e4b6f4af8c695c0341776db12b2f8e92bb561a
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-15 10:16:13 -0300

    Refatorando estrutura de erros para usar errors.New ao invés de structs.

--- src/models/modelsErros.go
+++ src/models/modelsErros.go
@@ -1,30 +1,21 @@
 // Package models modela tanto erros quanto entradas no Postgres/gPRC e afins, para que seja mais manutenível.
 package models
 
-import "log"
-
-type GerenciadorErros struct {
-	Mensagem string
-}
-
-func (e GerenciadorErros) Log(err error) {
-	log.Printf("⚠️ %s | Detalhes: %v", e.Mensagem, err)
-}
-
-func (e GerenciadorErros) Fatal(err error) {
-	log.Fatalf("❌ FATAL: %s | Detalhes: %v", e.Mensagem, err)
-}
+import "errors"
 
 // Erros PostgreSQL
 var (
 	// Erros Setup e migração
-	ErroAberturaPostgres = GerenciadorErros{"Erro ao tentar abrir o Postgres: "}
-	ErroConexaoPostgres  = GerenciadorErros{"Erro ao tentar conectar ao Postgres: "}
-	ErroMigracaoPostgres = GerenciadorErros{"Erro ao tentar migrar o Postgres: "}
+	ErroAberturaPostgres = errors.New("erro ao tentar abrir o Postgres: ")
+	ErroConexaoPostgres  = errors.New("erro ao tentar conectar ao Postgres: ")
+	ErroMigracaoPostgres = errors.New("erro ao tentar migrar o Postgres: ")
 
 	// Erros de Criação
-	ErroEntradaPostgres = GerenciadorErros{"Erro ao criar a entrada: "}
+	ErroEntradaPostgres = errors.New("erro ao criar a entrada: ")
 
 	// Erros de Deleção
-	ErroDeletePostgres = GerenciadorErros{"Erro ao deletar item: "}
+	ErroDeletePostgres = errors.New("erro ao deletar item: ")
+
+	// Erros de Procura
+	ErroBuscaPostgresNEncontrado = errors.New("não foram encontrados itens: ")
 )
