c68f4da2218ccf94f5a9c317ed57c4548becbc5e
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-15 10:40:19 -0300

    Criando API Externa RESTful genérica para o projeto.

---
+++ src/extAPI/extAPI.go
@@ -0,0 +1,8 @@
+// Package extAPi cuida da API RESTful externa, para o HTMX.
+package extAPI
+
+import "net/http"
+
+func SetupExtRoutes() { // Ainda não possuo os requisitos pro projeto então vou deixar aqui pronto kkkkkkkkkkk
+	mux := http.NewServeMux()
+}
