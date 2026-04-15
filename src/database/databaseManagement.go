0dfbf1567e4c06d9de430ff046483d73ffc64b83
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-15 10:21:23 -0300

    Refatoração da função ConectarDatabasePostgres para que tente mais de uma vez a conectar.

--- src/database/databaseManagement.go
+++ src/database/databaseManagement.go
@@ -2,18 +2,28 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"time"
+
+	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
 )
 
 var DB *sql.DB
 
-func ConectarDatabasePostgres() error {
+func ConectarDatabasePostgres() (*sql.DB, error) {
 	var err error
 	const dsn string = "host=pg_projeto_db user=user password=password dbname=shortener sslmode=disable"
 	DB, err = sql.Open("pgx", dsn)
 	if err != nil {
-		log.Fatal(err)
-		return err
+		return nil, fmt.Errorf("%w: %v", models.ErroAberturaPostgres, err)
 	}
-	err
+	for i := range 5 {
+		if err = DB.Ping(); err == nil {
+			return DB, nil
+		}
+		log.Printf("Aguardando Postgres: (Tentativa %d/5)", i+1)
+		time.Sleep(2 * time.Second)
+	}
+	return nil, fmt.Errorf("%w: %v", models.ErroConexaoPostgres, err)
 }
