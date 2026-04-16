ffa530e3844f874a01f78f23661139fc2a14bf2a
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-16 19:27:05 -0300

    feat: Adicionando o começo do teste de userManagement

---
+++ tests/database/userManagement_test.go
@@ -0,0 +1,21 @@
+package testpostgres
+
+import "testing"
+
+func TestPostgresUsuario(t *testing.T) {
+	t.Run("Criar entrada", func(t *testing.T) {
+		// teste de entrada...
+	})
+	t.Run("Ler todos os usuários", func(t *testing.T) {
+		// teste de leitura geral...
+	})
+	t.Run("Ler usuário específico", func(t *testing.T) {
+		// teste de leitura específico...
+	})
+	t.Run("Atualizar usuário específico", func(t *testing.T) {
+		// teste de atualizar usuários...
+	})
+	t.Run("Deletar usuário", func(t *testing.T) {
+		// Teste de deletar usuários...
+	})
+}
