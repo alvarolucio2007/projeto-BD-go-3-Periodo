3e45c0841e9843cd925604a69a942fb884d27c27
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-16 19:31:51 -0300

    update: Atualizando os tipos dos IDs de int32 para uint32

--- src/database/userManagement.go
+++ src/database/userManagement.go
@@ -6,14 +6,14 @@ import (
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
 )
 
-func CriarEntradaUsuario(user models.Usuario) (int32, error) {
-	var id int32
+func CriarEntradaUsuario(user models.Usuario) (uint32, error) {
+	var id uint32
 	query := "INSERT INTO usuarios (username,password,role) VALUES ($1,$2,$3) RETURNING id;"
 	err := DB.QueryRow(query, user.Username, user.Password, user.Role).Scan(&id)
 	if err != nil {
 		return 0, fmt.Errorf("%w: %v", models.ErroEntradaPostgres, err)
 	}
-	return id, nil
+	return uint32(id), nil
 }
 
 func LerTodosUsuarios() ([]models.Usuario, error) {
@@ -37,15 +37,15 @@ func LerTodosUsuarios() ([]models.Usuario, error) {
 	return usuarios, nil
 }
 
-func UpdateUsuarios(id int32, dados models.Usuario) error {
+func UpdateUsuarios(dados models.Usuario) error {
 	query := `UPDATE usuarios SET username=$1, password=$2, role=$3 WHERE id=$4`
-	if _, err := DB.Exec(query, dados.Username, dados.Password, dados.Role, id); err != nil {
+	if _, err := DB.Exec(query, dados.Username, dados.Password, dados.Role, dados.ID); err != nil {
 		return fmt.Errorf("%w: %v", models.ErroAtualizacaoPostgres, err)
 	}
 	return nil
 }
 
-func DeleteUsuarios(id int32) error {
+func DeleteUsuarios(id uint32) error {
 	query := `DELETE FROM usuarios WHERE id=$1`
 	res, err := DB.Exec(query, id)
 	if err != nil {
