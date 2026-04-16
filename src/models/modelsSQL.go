1bb093714a0abf2b087339a4b360e578577dcb42
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-16 19:33:59 -0300

    update: Atualizando os tipos dos IDs das structs de int32 para uint32

--- src/models/modelsSQL.go
+++ src/models/modelsSQL.go
@@ -3,21 +3,21 @@ package models
 import "time"
 
 type Usuario struct {
-	ID       int32
+	ID       uint32
 	Username string
 	Password string
 	Role     string
 }
 type Provas struct {
-	ID           int32
+	ID           uint32
 	NomeProva    string
 	TurmaProva   string
 	MateriaProva string
 	DataProva    time.Time
 }
 type Notas struct {
-	ID        int32
-	UsuarioID int32
-	ProvaID   int32
+	ID        uint32
+	UsuarioID uint32
+	ProvaID   uint32
 	NotaProva float32
 }
