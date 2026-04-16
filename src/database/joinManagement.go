c8131d4a9f9a6dc0727d5edcadf5e0f4bc5cf10b
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-16 18:27:48 -0300

    Ajustando structs e funções de JOIN para que aceitem a data da prova.

--- src/database/joinManagement.go
+++ src/database/joinManagement.go
@@ -4,21 +4,23 @@ import (
 	"database/sql"
 	_ "embed"
 	"fmt"
+	"time"
 
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
 )
-
-type LeftJoinType struct {
-	Username  string
-	NomeProva sql.NullString
-	NotaProva sql.NullFloat64
-}
 
 //go:embed sql/innerJoin.sql
 var innerJoinSQL string
 
 //go:embed sql/leftJoin.sql
 var leftJoinSQL string
+
+type LeftJoinType struct {
+	Username      string
+	NomeProva     sql.NullString
+	NotaProva     sql.NullFloat64
+	DataAplicacao time.Time
+}
 
 func LeftJoin() ([]LeftJoinType, error) {
 	rows, err := DB.Query(leftJoinSQL)
@@ -29,7 +31,7 @@ func LeftJoin() ([]LeftJoinType, error) {
 	var listaLeftJoins []LeftJoinType
 	for rows.Next() {
 		var r LeftJoinType
-		if err := rows.Scan(&r.Username, &r.NomeProva, &r.NotaProva); err != nil {
+		if err := rows.Scan(&r.Username, &r.NomeProva, &r.NotaProva, &r.DataAplicacao); err != nil {
 			return nil, fmt.Errorf("%w: %v", models.ErroLeftJoinScanPostgres, err)
 		}
 		listaLeftJoins = append(listaLeftJoins, r)
@@ -38,9 +40,10 @@ func LeftJoin() ([]LeftJoinType, error) {
 }
 
 type InnerJoinType struct {
-	Username  string
-	NomeProva string
-	NotaProva float32
+	Username      string
+	NomeProva     string
+	NotaProva     float32
+	DataAplicacao time.Time
 }
 
 func InnerJoin() ([]InnerJoinType, error) {
@@ -52,7 +55,7 @@ func InnerJoin() ([]InnerJoinType, error) {
 	var listaInnerJoins []InnerJoinType
 	for rows.Next() {
 		var i InnerJoinType
-		if err := rows.Scan(&i.Username, &i.NomeProva, &i.NotaProva); err != nil {
+		if err := rows.Scan(&i.Username, &i.NomeProva, &i.NotaProva, &i.DataAplicacao); err != nil {
 			return nil, fmt.Errorf("%w: %v", models.ErroInnerJoinScanPosgres, err)
 		}
 		listaInnerJoins = append(listaInnerJoins, i)
