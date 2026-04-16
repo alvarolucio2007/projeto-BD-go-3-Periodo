446ecf61ac720f1a60d20aad3121834d5452786e
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-16 08:33:57 -0300

    update: Atualuzando espaçamento para melhor leitura do query, ele em si não foi mudado.

--- src/sql/leftJoin.sql
+++ src/sql/leftJoin.sql
@@ -1,1 +1,4 @@
-SELECT usuarios.username,notas.nota_prova,provas.nome_prova FROM usuarios LEFT JOIN notas on notas.usuario_id=usuarios.id LEFT JOIN provas on provas.id=notas.prova_id;
+SELECT usuarios.username,notas.nota_prova,provas.nome_prova 
+FROM usuarios 
+LEFT JOIN notas on notas.usuario_id=usuarios.id 
+LEFT JOIN provas on provas.id=notas.prova_id;
