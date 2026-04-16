ab6b88106b7f9fcb6e92984b8039ec0e9edf5d4d
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-16 18:14:57 -0300

    Ajustando JOINS para mostrarem notas de aplicação das provas.

--- src/database/sql/innerJoin.sql
+++ src/database/sql/innerJoin.sql
@@ -1,4 +1,4 @@
-SELECT usuarios.username,provas.nome_prova,notas.nota_prova 
+SELECT usuarios.username,provas.nome_prova,notas.nota_prova,provas.data_aplicacao
 FROM usuarios 
 INNER JOIN notas ON usuarios.id=notas.usuario_id 
 INNER JOIN provas on provas.id=notas.prova_id;
--- src/database/sql/leftJoin.sql
+++ src/database/sql/leftJoin.sql
@@ -1,4 +1,4 @@
-SELECT usuarios.username,provas.nome_prova,notas.nota_prova
+SELECT usuarios.username,provas.nome_prova,notas.nota_prova,provas.data_aplicacao
 FROM usuarios 
 LEFT JOIN notas on notas.usuario_id=usuarios.id 
 LEFT JOIN provas on provas.id=notas.prova_id;
