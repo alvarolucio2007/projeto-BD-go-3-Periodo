a46ed916cb1472dedb61de77bec3321be2e489ac
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-16 18:30:50 -0300

    fix: Ajustando SQL para "bater" com o novo data_prova

--- src/database/sql/leftJoin.sql
+++ src/database/sql/leftJoin.sql
@@ -1,4 +1,4 @@
-SELECT usuarios.username,provas.nome_prova,notas.nota_prova,provas.data_aplicacao
+SELECT usuarios.username,provas.nome_prova,notas.nota_prova,provas.data_prova
 FROM usuarios 
 LEFT JOIN notas on notas.usuario_id=usuarios.id 
 LEFT JOIN provas on provas.id=notas.prova_id;
