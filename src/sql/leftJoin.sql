b8791a9d6701863e62a493e96d267323d81d9707
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-16 08:48:00 -0300

    update: Atualizando ordem das colunas (invertendo a ordem do nome das provas e das notas)

--- src/sql/leftJoin.sql
+++ src/sql/leftJoin.sql
@@ -1,4 +1,4 @@
-SELECT usuarios.username,notas.nota_prova,provas.nome_prova 
+SELECT usuarios.username,provas.nome_prova,notas.nota_prova
 FROM usuarios 
 LEFT JOIN notas on notas.usuario_id=usuarios.id 
 LEFT JOIN provas on provas.id=notas.prova_id;
