45ad826818a5f9ae571cd4084a250758c480dcbc
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-16 08:15:47 -0300

    fix: Atualizando comando, escrevi errado.

--- src/sql/leftJoin.sql
+++ src/sql/leftJoin.sql
@@ -1,1 +1,1 @@
-SELECT usuarios.username,notas.nota_prova FROM usuarios LEFT JOIN notas on notas.usuario_id=usuarios.idSELECT usuarios.username,notas.nota_prova,provas.nome_prova FROM usuarios LEFT JOIN notas on notas.usuario_id=usuarios.id LEFT JOIN provas on provas.id=notas.prova_id;
+SELECT usuarios.username,notas.nota_prova,provas.nome_prova FROM usuarios LEFT JOIN notas on notas.usuario_id=usuarios.id LEFT JOIN provas on provas.id=notas.prova_id;
