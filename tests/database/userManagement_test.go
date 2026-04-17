7167208320f5a12efbab2a12d67eec3090f96208
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-16 22:25:52 -0300

    feat: Criação de testes de Usuário.

--- tests/database/userManagement_test.go
+++ tests/database/userManagement_test.go
@@ -1,6 +1,11 @@
 package testpostgres
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/database"
+	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
+)
 
 func TestPostgresUsuario(t *testing.T) {
 	t.Run("Criar entrada", func(t *testing.T) {
@@ -17,5 +22,25 @@ func TestPostgresUsuario(t *testing.T) {
 	})
 	t.Run("Deletar usuário", func(t *testing.T) {
 		// Teste de deletar usuários...
+	})
+}
+
+var listaUsuario = []models.Usuario{
+	{Username: "Primeiro", Password: "Senha", Role: "Admin"},
+	{Username: "Segundo", Password: "SenhaDois", Role: "Estudante"},
+	{Username: "Terceiro", Password: "SenhaTres", Role: "Professor"},
+}
+
+func TestCriarUsuario(t *testing.T) {
+	t.Run("Criar usuário", func(t *testing.T) {
+		for i, u := range listaUsuario {
+			id, err := database.CriarEntradaUsuario(u)
+			if err != nil {
+				t.Errorf("Erro ao adicionar usuário: %v", err)
+			}
+			if id != uint32(i+1) {
+				t.Errorf("ID é diferente do esperado! Esperava %d, recebi %d", id, i+1)
+			}
+		}
 	})
 }
