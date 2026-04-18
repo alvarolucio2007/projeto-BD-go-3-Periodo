213e203eb3f2a32e088b2609eb628ac629fbe886
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-18 17:28:38 -0300

    feat: Criando função ReadAll para gRPC

--- src/gRPC/server/provaManagementServer.go
+++ src/gRPC/server/provaManagementServer.go
@@ -9,9 +9,10 @@ import (
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func (s *ServerNota) Create(ctx context.Context, in *proto.CreateProvaRequest) (*proto.CreateProvaResponse, error) {
+func (s *ServerProva) Create(ctx context.Context, in *proto.CreateProvaRequest) (*proto.CreateProvaResponse, error) {
 	log.Printf("função criar prova foi chamada com %v\n", in)
 	modeloProva := models.Provas{NomeProva: in.NomeProva, TurmaProva: in.TurmaProva, MateriaProva: in.MateriaProva, DataProva: in.DataProva.AsTime()}
 	id, err := database.CriarEntradaProva(modeloProva)
@@ -20,5 +21,26 @@ func (s *ServerNota) Create(ctx context.Context, in *proto.CreateProvaRequest) (
 	}
 	return &proto.CreateProvaResponse{
 		IdProva: id,
+	}, nil
+}
+
+func (s *ServerProva) ReadAll(ctx context.Context) (*proto.ProvaLista, error) {
+	log.Printf("Função ler todas as provas foi chamada")
+	provas, err := database.LerTodasProvas()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Erro ao procurar todas as provas: %v", err)
+	}
+	listaProvas := make([]*proto.Prova, 0, len(provas))
+	for _, p := range provas {
+		listaProvas = append(listaProvas, &proto.Prova{
+			Id:           int32(p.ID),
+			NomeProva:    p.NomeProva,
+			TurmaProva:   p.TurmaProva,
+			MateriaProva: p.MateriaProva,
+			DataProva:    timestamppb.New(p.DataProva),
+		})
+	}
+	return &proto.ProvaLista{
+		Provas: listaProvas,
 	}, nil
 }
