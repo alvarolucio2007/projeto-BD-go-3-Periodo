f2ed2af821079c43e2d4e00f3b250a36a5fbe6f0
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-21 09:41:24 -0300

    feat: Adicionando doReadAllProvas

--- src/gRPC/client/provaManagementClient.go
+++ src/gRPC/client/provaManagementClient.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/gRPC/proto"
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -17,4 +18,25 @@ func (h *HubConexoes) doCreateProva(prova *models.Provas) (uint32, error) {
 		return 0, err
 	}
 	return res.IdProva, nil
+}
+
+func (h *HubConexoes) doReadAllProva() ([]*models.Provas, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	res, err := h.Prova.ReadAll(ctx, &emptypb.Empty{})
+	if err != nil {
+		return nil, err
+	}
+	result := make([]*models.Provas, 0, len(res.Provas))
+	for _, u := range res.Provas {
+		prova := models.Provas{
+			ID:           uint32(u.Id),
+			NomeProva:    u.NomeProva,
+			TurmaProva:   u.TurmaProva,
+			MateriaProva: u.MateriaProva,
+			DataProva:    u.DataProva.AsTime(),
+		}
+		result = append(result, &prova)
+	}
+	return result, nil
 }
