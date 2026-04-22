03672ba4e8000e8982f9dc87b24857fe3f817750
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-22 17:51:56 -0300

    update: Atualizando net/http para o framework Gin

--- src/api/HTMX_API.go
+++ src/api/HTMX_API.go
@@ -1,8 +1,11 @@
 // Package api cuida da API RESTful externa, para o HTMX.
 package api
 
-import "net/http"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func SetupExtRoutes() { // Ainda não possuo os requisitos pro projeto então vou deixar aqui pronto kkkkkkkkkkk
-	mux := http.NewServeMux()
+	gin.SetMode(gin.DebugMode)
+	r := gin.Default()
 }
