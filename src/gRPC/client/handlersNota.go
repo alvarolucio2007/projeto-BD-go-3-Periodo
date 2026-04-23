0754c4af253b9df4e1a1045efaf5fa9673589591
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-23 18:58:02 -0300

    update: Finalizando arquivo handlersNota para que seja compatível com HTMX.

--- src/gRPC/client/handlersNota.go
+++ src/gRPC/client/handlersNota.go
@@ -1,6 +1,7 @@
 package grpcclient
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -10,16 +11,28 @@ import (
 
 func (h *HubConexoes) HandlerAddNota(c *gin.Context) {
 	var novaNota models.Notas
-	if err := c.ShouldBindJSON(&novaNota); err != nil {
+	notaNumeroString := c.PostForm("nota_prova")
+	notaProva, err := strconv.ParseFloat(notaNumeroString, 64)
+	if err != nil {
 		SendError(c, err)
 		return
 	}
+	novaNota.NotaProva = float32(notaProva)
+	pID, errP := strconv.ParseUint(c.PostForm("id_prova"), 10, 32)
+	uID, errU := strconv.ParseUint(c.PostForm("id_usuario"), 10, 32)
+	if errP != nil || errU != nil {
+		SendError(c, fmt.Errorf("id de prova ou usuário inválido"))
+		return
+	}
+	novaNota.ProvaID = uint32(pID)
+	novaNota.UsuarioID = uint32(uID)
+
 	id, err := h.DoCreateNota(&novaNota)
 	if err != nil {
 		SendError(c, err)
 		return
 	}
-	c.JSON(http.StatusCreated, gin.H{
+	c.HTML(http.StatusCreated, "add_nota.html", gin.H{
 		"message": "Nota criada com sucesso",
 		"id":      id,
 	})
@@ -33,11 +46,13 @@ func (h *HubConexoes) HandlerReadNota(c *gin.Context) {
 		return
 	}
 	if len(res) == 0 {
-		c.JSON(http.StatusNotFound, gin.H{
+		c.HTML(http.StatusNotFound, "read_notas.html", gin.H{
 			"message": "não foram encontradas notas",
+			"notas":   res,
 		})
+		return
 	}
-	c.JSON(http.StatusOK, gin.H{
+	c.HTML(http.StatusOK, "read_notas.html", gin.H{
 		"message": "Busca de nota feita com sucesso",
 		"notas":   res,
 	})
@@ -45,7 +60,7 @@ func (h *HubConexoes) HandlerReadNota(c *gin.Context) {
 
 func (h *HubConexoes) HandlerUpdateNota(c *gin.Context) {
 	var novaNota models.Notas
-	if err := c.ShouldBindJSON(&novaNota); err != nil {
+	if err := c.ShouldBind(&novaNota); err != nil {
 		SendError(c, err)
 		return
 	}
@@ -54,13 +69,13 @@ func (h *HubConexoes) HandlerUpdateNota(c *gin.Context) {
 		SendError(c, err)
 		return
 	}
-	c.JSON(http.StatusCreated, gin.H{
-		"message": "Nota criada com sucesso",
+	c.HTML(http.StatusOK, "nota_linha", gin.H{
+		"message": "Nota atualizada com sucesso",
 	})
 }
 
 func (h *HubConexoes) HandlerDeleteNota(c *gin.Context) {
-	id := c.Param("id")
+	id := c.PostForm("id")
 	idUint, err := strconv.ParseUint(id, 10, 32)
 	if err != nil {
 		SendError(c, err)
@@ -71,7 +86,7 @@ func (h *HubConexoes) HandlerDeleteNota(c *gin.Context) {
 		SendError(c, err)
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{
+	c.HTML(http.StatusOK, "delete_nota", gin.H{
 		"message": "Nota deletada com sucesso",
 	})
 }
