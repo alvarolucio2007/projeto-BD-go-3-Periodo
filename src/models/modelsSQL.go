b497a5033570adec368d2fc0dd3696418c9e05f5
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-25 12:09:56 -0300

    feat: Colocando tags nos modelos, para se conectarem ao JSON de maneira mais organizada.

--- src/models/modelsSQL.go
+++ src/models/modelsSQL.go
@@ -5,38 +5,43 @@ import (
 )
 
 type Usuario struct {
-	ID       uint32 `json:"id" form:"id"`
-	Username string `json:"username" form:"username"`
-	Password string `json:"password" form:"password"`
-	Role     string `json:"role" form:"role"`
+	ID       uint32 `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Role     string `json:"role"`
 }
+
 type Provas struct {
-	ID           uint32
-	NomeProva    string
-	TurmaProva   string
-	MateriaProva string
-	DataProva    time.Time
+	ID           uint32    `json:"id"`
+	NomeProva    string    `json:"nome_prova"`
+	TurmaProva   string    `json:"turma_prova"`
+	MateriaProva string    `json:"materia_prova"`
+	DataProva    time.Time `json:"data_prova"`
 }
+
 type Notas struct {
-	ID        uint32
-	UsuarioID uint32
-	ProvaID   uint32
-	NotaProva float32
+	ID        uint32  `json:"id"`
+	UsuarioID uint32  `json:"usuario_id"`
+	ProvaID   uint32  `json:"prova_id"`
+	NotaProva float32 `json:"nota_prova"`
 }
+
 type InnerJoinType struct {
-	Username      string
-	NomeProva     string
-	NotaProva     float32
-	DataAplicacao time.Time
+	Username      string    `json:"username"`
+	NomeProva     string    `json:"nome_prova"`
+	NotaProva     float32   `json:"nota_prova"`
+	DataAplicacao time.Time `json:"data_aplicacao"`
 }
+
 type LeftJoinType struct {
-	Username      string
-	NomeProva     *string
-	NotaProva     *float32
-	DataAplicacao *time.Time
+	Username      string     `json:"username"`
+	NomeProva     *string    `json:"nome_prova"`
+	NotaProva     *float32   `json:"nota_prova"`
+	DataAplicacao *time.Time `json:"data_aplicacao"`
 }
+
 type AuthResult struct {
-	Status   bool
-	Mensagem string
-	Role     string
+	Status   bool   `json:"status"`
+	Mensagem string `json:"mensagem"`
+	Role     string `json:"role"`
 }
