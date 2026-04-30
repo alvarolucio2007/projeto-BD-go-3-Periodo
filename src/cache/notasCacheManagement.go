1f4d042956aa7ffe28465077548d62cb0b55bfab
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-30 20:12:40 -0300

    feat: Refatorando a lógica de Ids do cache>

--- src/cache/notasCacheManagement.go
+++ src/cache/notasCacheManagement.go
@@ -1,17 +1,17 @@
 package cache
 
 import (
-	"strconv"
+	"fmt"
 
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
 )
 
 func AdicionarNotaRedis(codigo uint32, nota models.Notas) error {
-	codigoStr := strconv.FormatUint(uint64(codigo), 32)
+	codigoStr := fmt.Sprintf("nota:%s", codigo)
 	return RedisClient.Set(Ctx, codigoStr, nota, 0).Err()
 }
 
 func LerNotaRedis(codigo uint32) (models.Usuario, error) {
-	codigoStr := strconv.FormatUint(uint64(codigo), 32)
+	codigoStr := fmt.Sprintf("nota:%s", codigo)
 	return RedisClient.Get(Ctx, codigoStr).Result
 }
