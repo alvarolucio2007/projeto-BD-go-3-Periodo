7e1f0a26a7ade8f5d65e8fcfbb07030ee40ae55c
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-30 19:42:17 -0300

    feat: Implementando conversçao de códigos para string em ambas funções.

--- src/cache/userCacheManagement.go
+++ src/cache/userCacheManagement.go
@@ -1,11 +1,17 @@
 package cache
 
-import "github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
+import (
+	"strconv"
+
+	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
+)
 
 func AdicionarUsuarioRedis(codigo uint32, usuario models.Usuario) error {
-	return RedisClient.Set(Ctx, codigo, usuario, 0).Err()
+	codigoStr := strconv.FormatUint(uint64(codigo), 32)
+	return RedisClient.Set(Ctx, codigoStr, usuario, 0).Err()
 }
 
 func LerUsuarioRedis(codigo uint32) (models.Usuario, error) {
-	return RedisClient.Get(Ctx, codigo).Result()
+	codigoStr := strconv.FormatUint(uint64(codigo), 32)
+	return RedisClient.Get(Ctx, codigoStr).Result()
 }
