7401ba56054085a108e146cf714d1eaaeef41f59
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-30 20:12:57 -0300

    feat: Refatorando a lógica de IDs no cache.

--- src/cache/userCacheManagement.go
+++ src/cache/userCacheManagement.go
@@ -1,17 +1,17 @@
 package cache
 
 import (
-	"strconv"
+	"fmt"
 
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
 )
 
 func AdicionarUsuarioRedis(codigo uint32, usuario models.Usuario) error {
-	codigoStr := strconv.FormatUint(uint64(codigo), 32)
+	codigoStr := fmt.Sprintf("nota:%s", codigo)
 	return RedisClient.Set(Ctx, codigoStr, usuario, 0).Err()
 }
 
 func LerUsuarioRedis(codigo uint32) (models.Usuario, error) {
-	codigoStr := strconv.FormatUint(uint64(codigo), 32)
+	codigoStr := fmt.Sprintf("nota:%s", codigo)
 	return RedisClient.Get(Ctx, codigoStr).Result()
 }
