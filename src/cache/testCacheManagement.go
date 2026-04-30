9f166c5ab2a41a9164940a988bad4653152d4e0a
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-30 20:21:23 -0300

    feat: Adicionando lógica de marshal de JSON para adicionar no Redis, e ajustando Sprintf de nota para test.

--- src/cache/testCacheManagement.go
+++ src/cache/testCacheManagement.go
@@ -1,17 +1,23 @@
 package cache
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
 )
 
-func AdicionarTestRedis(codigo uint32, test models.Provas) {
-	codigoStr := fmt.Sprintf("nota:%s", codigo)
-	return RedisClient.Set(Ctx, codigoStr, test, 0).Err()
+func AdicionarTestRedis(codigo uint32, test models.Provas) error {
+	codigoStr := fmt.Sprintf("test:%s", codigo)
+
+	jsonData, err := json.Marshal(test)
+	if err != nil {
+		return err
+	}
+	return RedisClient.Set(Ctx, codigoStr, jsonData, 0).Err()
 }
 
 func LerTestRedis(codigo uint32) (models.Provas, error) {
-	codigoStr := fmt.Sprintf("nota:%s", codigo)
+	codigoStr := fmt.Sprintf("test:%s", codigo)
 	return RedisClient.Get(Ctx, codigoStr).Result()
 }
