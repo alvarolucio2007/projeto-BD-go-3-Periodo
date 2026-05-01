aa7348d508d7613693d6f71cd3c97ed91fe88e83
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-30 22:50:42 -0300

    feat: Refatorando função de adicionar usuario e ler usuario

--- src/cache/userCacheManagement.go
+++ src/cache/userCacheManagement.go
@@ -9,18 +9,24 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-func AdicionarUsuarioRedis(Ctx context.Context, rdb *redis.Client, codigo uint32, usuario models.Usuario) error {
+func AdicionarUsuarioRedis(Ctx context.Context, rdb *redis.Client, codigo uint32, usuario *models.Usuario) error {
 	codigoStr := fmt.Sprintf("user:%s", codigo)
-	jsonData, err := json.Marshal(usuario)
+	jsonData, err := json.Marshal(&usuario)
 	if err != nil {
 		return err
 	}
 	return rdb.Set(Ctx, codigoStr, jsonData, 0).Err()
 }
 
-func LerUsuarioRedis(Ctx context.Context, rdb *redis.Client, codigo uint32) (models.Usuario, error) {
+func LerUsuarioRedis(Ctx context.Context, rdb *redis.Client, codigo uint32) (*models.Usuario, error) {
 	codigoStr := fmt.Sprintf("user:%s", codigo)
-	return RedisClient.Get(Ctx, codigoStr).Result()
+	res := RedisClient.Get(Ctx, codigoStr).Return()
+	var resFatorado *models.Usuario
+	err := json.Unmarshal(res, resFatorado)
+	if err != nil {
+		return nil, err
+	}
+	return resFatorado, nil
 }
 
 func DeletarUsuarioRedis(Ctx context.Context, rdb *redis.Client, codigo uint32) error {
