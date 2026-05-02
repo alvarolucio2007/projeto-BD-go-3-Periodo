f1265e85dd74f852598c0ad9aff1f34ddaa90ace
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-02 16:13:54 -0300

    update: Fazendo a função ConectarRedis retornar *redis.Client

--- src/cache/cacheManagement.go
+++ src/cache/cacheManagement.go
@@ -13,15 +13,16 @@ var (
 	Ctx         = context.Background()
 )
 
-func ConectarRedis() {
-	RedisClient = redis.NewClient(&redis.Options{
+func ConectarRedis() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
 		Addr:     "redis_proj_bd:6379",
 		Password: "",
 		DB:       0,
 	})
-	err := RedisClient.Ping(Ctx).Err()
+	err := rdb.Ping(Ctx).Err()
 	if err != nil {
 		panic(fmt.Sprintf("Não foi possível conectar ao Redis: %v", err))
 	}
 	fmt.Println("Redis conectado.")
+	return rdb
 }
