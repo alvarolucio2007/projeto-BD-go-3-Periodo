7cbb4effd7e535f2143e50e1050c41be68f1135d
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-03 17:02:37 -0300

    update: Atualizando import para adicionar versão /v9 em go-redis;

--- src/gRPC/client/handlersJoins.go
+++ src/gRPC/client/handlersJoins.go
@@ -6,7 +6,7 @@ import (
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/gRPC/proto"
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
 	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis"
+	"github.com/redis/go-redis/v9"
 )
 
 type LeftJoinHandler struct {
