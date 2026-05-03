3ea992091f93a25bf68bcc3f4cd429ee6c93e10b
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-02 21:58:59 -0300

    update: Removendo HubConexoes e ConnectAll para uma abordagem muito mais modular, com structs individuais.

--- src/gRPC/client/clientManagementGPRC.go
+++ src/gRPC/client/clientManagementGPRC.go
@@ -7,14 +7,6 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type HubConexoes struct {
-	User      proto.UsuariosServiceClient
-	Prova     proto.ProvaServiceClient
-	Nota      proto.NotaServiceClient
-	LeftJoin  proto.LeftJoinServiceClient
-	InnerJoin proto.InnerJoinServiceClient
-	Conn      *grpc.ClientConn
-}
 type UserConexao struct {
 	User proto.UsuariosServiceClient
 	Conn *grpc.ClientConn
@@ -36,17 +28,6 @@ type InnerJoinConexao struct {
 	Conn      *grpc.ClientConn
 }
 
-func ConnectAll(addr string) (*HubConexoes, error) {
-	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return nil, err
-	}
-	return &HubConexoes{
-		User:      proto.NewUsuariosServiceClient(conn),
-		Prova:     proto.NewProvaServiceClient(conn),
-		Nota:      proto.NewNotaServiceClient(conn),
-		InnerJoin: proto.NewInnerJoinServiceClient(conn),
-		LeftJoin:  proto.NewLeftJoinServiceClient(conn),
-		Conn:      conn,
-	}, nil
+func NewGRPCConn(addr string) (*grpc.ClientConn, error) {
+	return grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 }
