e008ad071906b3a6c539788a081ff51d3f659f18
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-04 10:28:33 -0300

    feat: Adicionando client DoReadAllUser

--- src/gRPC/client/clientsUserManagement.go
+++ src/gRPC/client/clientsUserManagement.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/gRPC/proto"
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (u *UserConexao) DoCreateUser(user *models.Usuario) (uint32, error) {
@@ -22,6 +23,26 @@ func (u *UserConexao) DoReadUser(username string) ([]*models.Usuario, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	res, err := u.User.Read(ctx, &proto.UsuarioReadRequest{Nome: username})
+	if err != nil {
+		return nil, err
+	}
+	result := make([]*models.Usuario, 0, len(res.Usuarios))
+	for _, u := range res.Usuarios {
+		user := models.Usuario{
+			ID:       u.Id,
+			Username: u.Username,
+			Password: u.Password,
+			Role:     u.Role,
+		}
+		result = append(result, &user)
+	}
+	return result, nil
+}
+
+func (u *UserConexao) DoReadAllUser(ctx context.Context) ([]*models.Usuario, error) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	res, err := u.User.ReadAll(ctx, &emptypb.Empty{})
 	if err != nil {
 		return nil, err
 	}
