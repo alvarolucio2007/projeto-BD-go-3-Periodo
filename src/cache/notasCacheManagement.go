27c16e50aacff35342033be9b296b02299a644df
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-06 20:54:44 -0300

    fix: Atualizando retorno de funções de []*models.Provas para []*models.InnerJoinType

--- src/cache/notasCacheManagement.go
+++ src/cache/notasCacheManagement.go
@@ -19,7 +19,7 @@ func AdicionarNotaRedis(Ctx context.Context, rdb *redis.Client, codigo uint32, n
 	return rdb.Set(Ctx, codigoStr, jsonData, 0).Err()
 }
 
-func AdicionarTodasNotasRedis(Ctx context.Context, rdb *redis.Client, notas []*models.Provas) error {
+func AdicionarTodasNotasRedis(Ctx context.Context, rdb *redis.Client, notas []*models.InnerJoinType) error {
 	const cacheKey = "notas:all"
 	jsonData, err := json.Marshal(&notas)
 	if err != nil {
@@ -28,18 +28,29 @@ func AdicionarTodasNotasRedis(Ctx context.Context, rdb *redis.Client, notas []*m
 	return rdb.Set(Ctx, cacheKey, jsonData, 10*time.Minute).Err()
 }
 
-func LerNotaRedis(Ctx context.Context, rdb *redis.Client, codigo uint32) (*models.Notas, error) {
+func LerNotaRedis(Ctx context.Context, rdb *redis.Client, codigo uint32) (*models.InnerJoinType, error) {
 	codigoStr := fmt.Sprintf("nota:%d", codigo)
 	res, err := rdb.Get(Ctx, codigoStr).Result()
 	if err != nil {
 		return nil, err
 	}
-	var resFatorado models.Notas
+	var resFatorado models.InnerJoinType
 	err = json.Unmarshal([]byte(res), &resFatorado)
 	if err != nil {
 		return nil, err
 	}
 	return &resFatorado, nil
+}
+
+func LerAllNotaRedis(Ctx context.Context, rdb *redis.Client) ([]*models.InnerJoinType, error) {
+	const codigoStr = "notas:all"
+	res, err := rdb.Get(Ctx, codigoStr).Result()
+	if err != nil {
+		return nil, err
+	}
+	var resFatorado []*models.InnerJoinType
+	err = json.Unmarshal([]byte(res), &resFatorado)
+	return resFatorado, nil
 }
 
 func DeletarNotaRedis(Ctx context.Context, rdb *redis.Client, codigo uint32) error {
