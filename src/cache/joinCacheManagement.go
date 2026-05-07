c60c0e19f458d625a684f9fc3ccfecb852fdfac9
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-07 11:14:38 -0300

    feat: Adicionando funções para criar e ler no cache de InnerJoin.

---
+++ src/cache/joinCacheManagement.go
@@ -0,0 +1,33 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
+	"github.com/redis/go-redis/v9"
+)
+
+func AdicionarInnerJoinRedis(Ctx context.Context, rdb *redis.Client, innerJoins []*models.InnerJoinType) error {
+	const codigoStr = "innerJoin:all"
+	jsonData, err := json.Marshal(&innerJoins)
+	if err != nil {
+		return err
+	}
+	return rdb.Set(Ctx, codigoStr, jsonData, 10*time.Minute).Err()
+}
+
+func LerInnerJoinRedis(Ctx context.Context, rdb *redis.Client) ([]*models.InnerJoinType, error) {
+	const codigoStr = "innerJoin:all"
+	res, err := rdb.Get(Ctx, codigoStr).Result()
+	if err != nil {
+		return nil, err
+	}
+	var resFatorado []*models.InnerJoinType
+	err = json.Unmarshal([]byte(res), &resFatorado)
+	if err != nil {
+		return nil, err
+	}
+	return resFatorado, nil
+}
