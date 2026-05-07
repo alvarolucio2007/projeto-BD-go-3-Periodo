febabfd201b0bc4ff162915b13694c2b09f76c21
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-07 12:19:54 -0300

    feat: Adicionando funções do Redis de LeftJoin (Adicionar e Ler)

--- src/cache/joinCacheManagement.go
+++ src/cache/joinCacheManagement.go
@@ -31,3 +31,26 @@ func LerInnerJoinRedis(Ctx context.Context, rdb *redis.Client) ([]*models.InnerJ
 	}
 	return resFatorado, nil
 }
+
+func AdicionarLeftJoinRedis(Ctx context.Context, rdb *redis.Client, leftJoins []*models.LeftJoinType) error {
+	const codigoStr = "leftJoin:all"
+	jsonData, err := json.Marshal(&leftJoins)
+	if err != nil {
+		return err
+	}
+	return rdb.Set(Ctx, codigoStr, jsonData, 10*time.Minute).Err()
+}
+
+func LerLeftJoinRedis(Ctx context.Context, rdb *redis.Client) ([]*models.LeftJoinType, error) {
+	const codigoStr = "leftJoin:all"
+	res, err := rdb.Get(Ctx, codigoStr).Result()
+	if err != nil {
+		return nil, err
+	}
+	var resFatorado []*models.LeftJoinType
+	err = json.Unmarshal([]byte(res), &resFatorado)
+	if err != nil {
+		return nil, err
+	}
+	return resFatorado, nil
+}
