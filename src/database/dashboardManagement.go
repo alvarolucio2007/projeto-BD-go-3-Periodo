706c4a766669720d70aedf46327317267e9b7986
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-13 22:29:24 -0300

    feat: Adicionando função LerQuantidadeNotaProvaAluno, que será pra um gráfico de dispersão.

--- src/database/dashboardManagement.go
+++ src/database/dashboardManagement.go
@@ -23,6 +23,33 @@ func LerQuantidadeProvaAluno() (map[string]int, error) {
 	return stats, nil
 }
 
-func LerQuantidadeNotaProvaAluno() ([]models.EstatisticaAluno, error) {
-	query := "SELECT usuarios.username,COUNT(notas.id) as total_provas FROM usuarios LEFT JOIN notas ON usuarios.id=notas.usuario_id WHERE usuarios.role='aluno' GROUP BY usuarios.username ORDER BY total_provas DESC;"
+func LerQuantidadeNotaProvaAluno() (map[string]models.EstatisticaAluno, error) {
+	query := `SELECT 
+    u.username,
+    COUNT(n.id) AS total_provas,
+    COALESCE(AVG(n.nota), 0.0) AS media 
+		FROM usuarios u
+		LEFT JOIN notas n ON u.id = n.usuario_id
+		LEFT JOIN provas p ON p.id = n.prova_id
+		WHERE u.role = 'aluno' 
+		GROUP BY u.username 
+		ORDER BY total_provas ASC;`
+	rows, err := DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	result := make(map[string]models.EstatisticaAluno)
+	for rows.Next() {
+		var (
+			username   string
+			quantidade int
+			media      float64
+		)
+		if err := rows.Scan(&username, &quantidade, &media); err != nil {
+			return nil, err
+		}
+		result[username] = models.EstatisticaAluno{QuantidadeProva: quantidade, MediaProvas: media}
+	}
+	return result, nil
 }
