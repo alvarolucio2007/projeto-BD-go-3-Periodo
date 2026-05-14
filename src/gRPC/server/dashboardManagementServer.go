f747e33c97d60cf0e7612d33f6728bc393a50d5d
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-14 20:55:45 -0300

    feat: Adicionando servidores de QuantidadeProvaAluno e QuantidadeNotaProvaAluno

---
+++ src/gRPC/server/dashboardManagementServer.go
@@ -0,0 +1,38 @@
+package grpcserver
+
+import (
+	"context"
+	"log"
+
+	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/database"
+	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/gRPC/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *ServerDashboard) QuantidadeProvaAluno(ctx context.Context, in *proto.QuantidadeProvaAlunoRequest) (*proto.QuantidadeProvaAlunoResponse, error) {
+	log.Printf("função dashboard prova aluno request foi chamada com %v\n", in.NomeBusca)
+	res, err := database.LerQuantidadeProvaAluno(in.NomeBusca)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Erro ao fazer o relatório: %v", err)
+	}
+	return &proto.QuantidadeProvaAlunoResponse{
+		Response: res,
+	}, nil
+}
+
+func (s *ServerDashboard) QuantidadeNotaProvaAluno(ctx context.Context, in *proto.QuantidadeNotaProvaAlunoRequest) (*proto.QuantidadeNotaProvaAlunoResponse, error) {
+	log.Printf("função dashboard nota prova aluno foi chamada com %v\n", in.NomeBusca)
+	res, err := database.LerQuantidadeNotaProvaAluno(in.NomeBusca)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Erro ao fazer o relatório: %v", err)
+	}
+	resCorrect := make(map[string]*proto.EstatisticaAluno)
+	for a, b := range res {
+		c := &proto.EstatisticaAluno{QuantidadeProva: int64(b.QuantidadeProva), MediaProva: float32(b.MediaProvas)}
+		resCorrect[a] = c
+	}
+	return &proto.QuantidadeNotaProvaAlunoResponse{
+		Response: resCorrect,
+	}, nil
+}
