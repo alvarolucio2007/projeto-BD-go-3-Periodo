dd7c69987d5b8b22371cbef48613142b1b25adb3
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-13 21:58:50 -0300

    feat: Adicionando função LerQuantidadeProvaAluno, que retorna um hashmap

--- src/database/teste.go
+++ src/database/teste.go
@@ -1,1 +1,22 @@
 package database
+
+func LerQuantidadeProvaAluno() (map[string]int, error) {
+	query := "SELECT usuarios.username,COUNT(notas.id) as total_provas FROM usuarios LEFT JOIN notas ON usuarios.id=notas.usuario_id WHERE usuarios.role='aluno' GROUP BY usuarios.username ORDER BY total_provas DESC;"
+	rows, err := DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	stats := make(map[string]int)
+	for rows.Next() {
+		var (
+			user string
+			qtd  int
+		)
+		if err := rows.Scan(&user, &qtd); err != nil {
+			return nil, err
+		}
+		stats[user] = qtd
+	}
+	return stats, nil
+}
