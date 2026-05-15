57c72321ef9dc2347224f5216a62d7e9870ca9d4
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-15 19:02:17 -0300

    feat: Adicionanod arquivo de handlers de dashboard, mas farei o cache deles primeiro.

--- src/gRPC/client/handlersDashboard.go
+++ src/gRPC/client/handlersDashboard.go
@@ -1,1 +1,11 @@
 package grpcclient
+
+import (
+	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/gRPC/proto"
+	"github.com/redis/go-redis/v9"
+)
+
+type DashboardHandler struct {
+	Rdb             *redis.Client
+	DashboardClient proto.DashboardServiceClient
+}
