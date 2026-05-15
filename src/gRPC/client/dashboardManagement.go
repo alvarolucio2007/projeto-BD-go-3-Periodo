64281cd0c8cab0076dc1fc2a69daa9b308c98896
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-15 08:06:57 -0300

    feat: Adicionando arquivo dashboardManagement, client gRPC do dashboard, com uma função DoLerQuantidadeProvaAluno

---
+++ src/gRPC/client/dashboardManagement.go
@@ -0,0 +1,22 @@
+package grpcclient
+
+import (
+	"context"
+	"time"
+
+	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/gRPC/proto"
+)
+
+func (h *HubGeral) DoLerQuantidadeProvaAluno(nome string) (map[string]int64, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	res, err := h.Dashboard.QuantidadeProvaAluno(ctx, &proto.QuantidadeProvaAlunoRequest{NomeBusca: nome})
+	if err != nil {
+		return nil, err
+	}
+	result := make(map[string]int64)
+	for s, i := range res.Response {
+		result[s] = i
+	}
+	return result, nil
+}
