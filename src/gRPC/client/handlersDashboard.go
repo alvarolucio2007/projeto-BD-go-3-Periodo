b6088d69c4b0f48423a00e6a191b07ec1497557f
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-16 12:14:04 -0300

    feat: Adicionando HandlerMediaNotaMateria

--- src/gRPC/client/handlersDashboard.go
+++ src/gRPC/client/handlersDashboard.go
@@ -49,6 +49,7 @@ func (d *DashboardHandler) HandlerQuantidadeProvaAluno(c *gin.Context, hub *HubG
 		SendError(c, err)
 		return
 	}
+
 	c.JSON(http.StatusOK, res.Response)
 }
 
@@ -83,6 +84,43 @@ func (d *DashboardHandler) HandlerQuantidadeNotaProvaAluno(c *gin.Context, hub *
 		return
 	}
 
+	mapCerto := make(map[string]models.EstatisticaAluno)
+	for n, e := range res.Response {
+		mapCerto[n] = FromProto(e)
+	}
+	c.JSON(http.StatusOK, mapCerto)
+}
+
+func (d *DashboardHandler) HandlerMediaNotaMateria(c *gin.Context, hub *HubGeral) {
+	categoria := c.Query("categoria")
+	ctx := c.Request.Context()
+	if categoria == "" {
+		resRedis, err := cache.LerMediaNotaMaterias(ctx, d.Rdb)
+		if err != nil {
+			SendError(c, err)
+			return
+		}
+		if resRedis != nil {
+			c.JSON(http.StatusOK, resRedis)
+			go func(client proto.DashboardServiceClient) {
+				bgCtx := context.Background()
+				res, err := client.MediaNotaMateria(bgCtx, &proto.MediaNotaMateriaRequest{NomeCategoria: ""})
+				if err == nil {
+					mapCerto := make(map[string]models.EstatisticaAluno)
+					for n, e := range res.Response {
+						mapCerto[n] = FromProto(e)
+					}
+					_ = cache.AdicionarMediaNotaMaterias(bgCtx, d.Rdb, mapCerto)
+				}
+			}(hub.Dashboard)
+			return
+		}
+	}
+	res, err := hub.Dashboard.MediaNotaMateria(ctx, &proto.MediaNotaMateriaRequest{NomeCategoria: categoria})
+	if err != nil {
+		SendError(c, err)
+		return
+	}
 	mapCerto := make(map[string]models.EstatisticaAluno)
 	for n, e := range res.Response {
 		mapCerto[n] = FromProto(e)
