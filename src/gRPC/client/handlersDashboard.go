9ad53e11b50f2119bb7ede8563b34a94f6348137
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-05-16 11:55:54 -0300

    feat: Adicionando função HandlerLerQuantidadeNotaProvaAluno e auxiliar FromProto

--- src/gRPC/client/handlersDashboard.go
+++ src/gRPC/client/handlersDashboard.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/cache"
 	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/gRPC/proto"
+	"github.com/alvarolucio2007/projeto-DB-go-3-Periodo/src/models"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
 )
@@ -15,7 +16,14 @@ type DashboardHandler struct {
 	DashboardClient proto.DashboardServiceClient
 }
 
-func (d *DashboardHandler) HandlerLerQuantidadeProvaAluno(c *gin.Context, hub *HubGeral) {
+func FromProto(p *proto.EstatisticaAluno) models.EstatisticaAluno {
+	return models.EstatisticaAluno{
+		QuantidadeProva: int(p.QuantidadeProva),
+		MediaProvas:     float64(p.QuantidadeProva),
+	}
+}
+
+func (d *DashboardHandler) HandlerQuantidadeProvaAluno(c *gin.Context, hub *HubGeral) {
 	username := c.Query("username")
 	ctx := c.Request.Context()
 	if username == "" {
@@ -42,4 +50,42 @@ func (d *DashboardHandler) HandlerLerQuantidadeProvaAluno(c *gin.Context, hub *H
 		return
 	}
 	c.JSON(http.StatusOK, res.Response)
+}
+
+func (d *DashboardHandler) HandlerQuantidadeNotaProvaAluno(c *gin.Context, hub *HubGeral) {
+	username := c.Query("username")
+	ctx := c.Request.Context()
+	if username == "" {
+		resRedis, err := cache.LerQuantidadeNotaProvaAlunos(ctx, d.Rdb)
+		if err != nil {
+			SendError(c, err)
+			return
+		}
+		if resRedis != nil {
+			c.JSON(http.StatusOK, resRedis)
+			go func(client proto.DashboardServiceClient) {
+				bgCtx := context.Background()
+				res, err := client.QuantidadeNotaProvaAluno(bgCtx, &proto.QuantidadeNotaProvaAlunoRequest{NomeBusca: ""})
+				if err == nil {
+					mapCerto := make(map[string]models.EstatisticaAluno)
+					for n, e := range res.Response {
+						mapCerto[n] = FromProto(e)
+					}
+					_ = cache.AdicionarQuantidadeNotaProvaAlunos(bgCtx, d.Rdb, mapCerto)
+				}
+			}(hub.Dashboard)
+			return
+		}
+	}
+	res, err := hub.Dashboard.QuantidadeNotaProvaAluno(ctx, &proto.QuantidadeNotaProvaAlunoRequest{NomeBusca: username})
+	if err != nil {
+		SendError(c, err)
+		return
+	}
+
+	mapCerto := make(map[string]models.EstatisticaAluno)
+	for n, e := range res.Response {
+		mapCerto[n] = FromProto(e)
+	}
+	c.JSON(http.StatusOK, mapCerto)
 }
